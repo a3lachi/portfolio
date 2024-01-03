62c1714ef177f23b07872fd82d63f0e59898d6ea
a3laxi <mad.aalachi@gmail.com>
2024-01-03 18:46:49 +0100

    added target=blank to links in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,10 @@ const Footer = () => {
     return(
         <div  style={{  width:'100vw' , backgroundColor:'white' , zIndex:'3' , color:'black' , minHeight:'10vh' }} >
                 <div className={styles.contact}><p>Contact me by <a href="mailto:mad.aalachi@gmail.com">mail</a>
-<span className={styles.social}><a href="http://www.github.com/a3lachi"><img src={Github} width={20} /></a>
-<a href="https://www.linkedin.com/"><img src={Linkedin} width={20} /></a>
-<a href="https://www.youtube.com/channel/UC2A26hYnRVkNp1vn1eWSeCg"><img src={Youtube} width={20} /></a>
-<a href="https://twitter.com/a3laxi"><img src={Twitter} width={20} /></a></span>
+<span className={styles.social}><a target="_blank" href="http://www.github.com/a3lachi"><img src={Github} width={20} /></a>
+<a target="_blank" href="https://www.linkedin.com/"><img src={Linkedin} width={20} /></a>
+<a target="_blank" href="https://www.youtube.com/channel/UC2A26hYnRVkNp1vn1eWSeCg"><img src={Youtube} width={20} /></a>
+<a target="_blank" href="https://twitter.com/a3laxi"><img src={Twitter} width={20} /></a></span>
 <span className={styles.copyright}>© 2023 - 2024</span></p></div>
                 </div>
 
