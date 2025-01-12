725dc676b1723d7fc7ad33bb14fc6027369fdfca
a3laxi <mad.aalachi@gmail.com>
2025-01-12 14:41:59 +0100

    added Linkedin link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
         <div id="footer" style={{  width:'100vw' , backgroundColor:'white' , zIndex:'3' , color:'black' , minHeight:'10vh' }} >
                 <div className={styles.contact}><p>Contact me by <a href="mailto:mad.aalachi@gmail.com">mail</a>
 <span className={styles.social}><a target="_blank" href="http://www.github.com/a3lachi"><img src={Github} width={20} /></a>
-<a target="_blank" href="https://www.linkedin.com/"><img src={Linkedin} width={20} /></a>
+<a target="_blank" href="https://www.linkedin.com/in/mohamed-aalachi-878479252/"><img src={Linkedin} width={20} /></a>
 <a target="_blank" href="https://www.youtube.com/channel/UC2A26hYnRVkNp1vn1eWSeCg"><img src={Youtube} width={20} /></a>
 <a target="_blank" href="https://twitter.com/a3laxi"><img src={Twitter} width={19} /></a></span>
 <span className={styles.copyright}>© 2023 - 2025</span></p></div>
